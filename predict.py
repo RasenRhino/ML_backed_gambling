ebdc4f2b7cb9e4aa28dcdf2490268ed13dbd915b
Ridham Bhagat <ridhambhagat@Ridhams-iMac.local>
2020-06-13 02:38:12 +0530

    teams

--- predict.py
+++ predict.py
@@ -3,11 +3,15 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 dele=pd.read_csv("deliveries.csv")
 matches=pd.read_csv("matches.csv")
 matches['match_id'] = matches['id']
 matches = matches.drop('id', axis=1)
 df = dele.merge(matches, on='match_id', how='left')
+teams = set(df.team1.unique()).union(set(df.team2.unique()))
+players = set(df.batsman.unique()).union(set(df.bowler.unique())).union(set(df.non_striker.unique()))
+players=list(players)
 #encoders
 batenc = LabelEncoder()
 bowlenc=LabelEncoder()
@@ -30,13 +34,17 @@ df['will_be_out'] = y<0
 df['four']= (y==4)
 df['six']= (y==6)
 forest = RandomForestClassifier(n_jobs=-1)
+X = df[['batting_team_e','bowling_team_e','batsman_e', 'non_striker_e', 'bowler_e', 'over', 'ball',
+        'inning']]
+# print(cross_val_score(forest, X, df.will_be_out, cv=10, scoring='roc_auc',n_jobs=-1).mean())
+x='Sunrisers Hyderabad'
+def predict_out(bat_team,bowl_team,season,batsman,nonstriker,bowler,over,bowl_no,inning):
+	train_data=df[(df['season']!=2008)&(df['season']!=2009)]
+	X1=train_data[['batting_team_e','bowling_team_e','batsman_e', 'non_striker_e', 'bowler_e', 'over', 'ball',
+        'inning']]
+    y1=train_data['will_be_out']
+	prob=cross_val_score(forest, train_data, train_data.will_be_out, cv=10, scoring='roc_auc',n_jobs=-1).mean()
+	X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size=0.2, random_state=40)
+	forest.fit(X_train,y_train)
+	forest.fit(X,df['will_be_out'])
 
-def predict(bat_team,bowl_team,season,batsman,nonstriker,bowler,over,bowl,inning):
-	match_data=df[(df['batting_team']==bat_team)&(df['bowling_team']==bowl_team)&(df['season']==season)]
-	players = set(match_data.batsman.unique()).union(set(match_data.bowler.unique())).union(set(match_data.non_striker.unique()))
-	player_encoder = LabelEncoder()
-	venue_encoder = LabelEncoder()
-	batenc = LabelEncoder()
-	bowlenc=LabelEncoder()
-	player_encoder.fit(players)
-	venue_encoder.fit(df.venue)
