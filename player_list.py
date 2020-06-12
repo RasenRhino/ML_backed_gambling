335094f62b9a076548f33f553a884e265ba66da3
Ridham Bhagat <ridhambhagat@Ridhams-iMac.local>
2020-06-13 01:21:26 +0530

    team_name

---
+++ player_list.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd 
+dele=pd.read_csv("deliveries.csv")
+def teamname_list():
+	teams = set(dele.team1.unique()).union(set(dele.team2.unique()))
+	return list(teams)
