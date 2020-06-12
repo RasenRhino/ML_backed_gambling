4b6fcb2d1736c645c07a3a9c2f60b7f681247da3
Ridham Bhagat <ridhambhagat@Ridhams-iMac.local>
2020-06-13 01:51:06 +0530

    uu

--- player_list.py
+++ player_list.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd 
 dele=pd.read_csv("deliveries.csv")
 matches=pd.read_csv("matches.csv")
+matches['match_id'] = matches['id']
+matches = matches.drop('id', axis=1)
 df = dele.merge(matches, on='match_id', how='left')
 def player_list_batsman(team,season):
 	players = set(df.batsman.unique()).union(set(df.bowler.unique())).union(set(df.non_striker.unique()))
 	return list(players)
-
