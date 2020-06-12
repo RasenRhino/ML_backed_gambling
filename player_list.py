5b18398a870424bbabc9c7abbd01479d40f902dd
Ridham Bhagat <ridhambhagat@Ridhams-iMac.local>
2020-06-13 04:24:26 +0530

    ha

--- player_list.py
+++ player_list.py
@@ -5,7 +5,8 @@ matches=pd.read_csv("matches.csv")
 matches['match_id'] = matches['id']
 matches = matches.drop('id', axis=1)
 df = dele.merge(matches, on='match_id', how='left')
-def player_list(team,season):
-	df=df[df['season']==season]
-	players = set(df.batsman.unique()).union(set(df.bowler.unique())).union(set(df.non_striker.unique()))
+def player_list_name(team1,team2,season):
+	abc=df[(df['season']==season)&(((df['batting_team']==team1)&(df['bowling_team']==team2)) | ((df['batting_team']==team2)&(df['bowling_team']==team1)))]
+	players = set(abc.batsman.unique()).union(set(abc.bowler.unique())).union(set(abc.non_striker.unique()))
 	return list(players)
+# print(len(player_list_name('Sunrisers Hyderabad','Mumbai Indians',2014)))
